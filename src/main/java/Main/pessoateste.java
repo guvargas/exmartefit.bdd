ce0fcb31cf74a572e0ecf6f2d4453e5df6b43d60
Vargas <gupontoandrade00@gmail.com>
2020-09-18 13:29:20 -0300

    Tela registrar aluno adc

--- src/main/java/Main/pessoateste.java
+++ src/main/java/Main/pessoateste.java
@@ -8,21 +8,23 @@ import Model.PessoaModel;
 
 public class pessoateste {
      public static void main(String[] args){
-        PessoaModel t = new PessoaModel("b", "b","ontem", "Instrutor", "Supino", "8 da manhã");
+      //PessoaModel(String nome, String cpf, String dataNascimento, String tipo, String treino, String horario) {
+
+        PessoaModel t = new PessoaModel("a", "a","18/10/2000", "Aluno", "Supino", "8 da manhã");
         PessoaDAO pDAO = new PessoaDAO();
         pDAO.gravar(t);
        System.out.println(t);
-//        PessoaModel f = new PessoaModel("Jose", "00","hoje", "nada", "32");
-//        PessoaDAO pDAOf = new PessoaDAO();
-//        pDAOf.gravar(f);
-//       System.out.println(f);
+        PessoaModel f = new PessoaModel("b", "b","10/10/2000","Nenhum", "Instrutor", "6 p.m");
+        PessoaDAO pDAOf = new PessoaDAO();
+        pDAOf.gravar(f);
+       System.out.println(f);
 
 //        PessoaDAO produtoRepository = new PessoaDAO();
 //
 //        PessoaModel produto = produtoRepository.buscarId(9);
 //
 //        System.out.println(produto.toString());
-//
+
 
     }
    
--- src/main/java/View/TelaLoginView.java
+++ src/main/java/View/TelaLoginView.java
@@ -1,8 +1,10 @@
 
 package View;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class TelaLoginView extends javax.swing.JFrame {
 
@@ -87,35 +89,40 @@ public class TelaLoginView extends javax.swing.JFrame {
     }//GEN-LAST:event_tfLoginActionPerformed
 
     public static void main(String args[]) {
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaLoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaLoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaLoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaLoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TelaLoginView().setVisible(true);
-            }
-        });
+         try {
+    UIManager.setLookAndFeel( new FlatDarkLaf() );
+} catch( Exception ex ) {
+    System.err.println( "Failed to initialize LaF" );
+}
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(TelaLoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(TelaLoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(TelaLoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(TelaLoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new TelaLoginView().setVisible(true);
+//            }
+//        });
+//    }
     }
-
      
     public String getLogin(){
         return tfLogin.getText();
